51d6cc2dd71112cd3875c5d60cc703c0dacd2843
RodolfiSimone <66484586+RodolfiSimone@users.noreply.github.com>
2021-02-09 16:12:19 +0100

    Update es 25_190.py

--- es 25_190.py
+++ es 25_190.py
@@ -7,6 +7,7 @@ import random
 import operator
 
 matricola_voti = {}
+voti_presenti = []
 
 for n in range(30):
     matricola_voti[n] = random.randint(1, 10,)
@@ -14,7 +15,6 @@ matricola_voti = dict( sorted(matricola_voti.items(), key=operator.itemgetter(1)
 for e in matricola_voti:
     print("il voto della matricola", e, "è", matricola_voti[e])
 
-voti_presenti = []
 for e in matricola_voti.values():
     if e not in voti_presenti:
         voti_presenti.append(e)
